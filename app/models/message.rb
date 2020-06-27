74f860faa14967b7d7b3bee6d54bf4ac0c496712
koyashika <u_wh.sunnnny.xx@softbank.ne.jp>
2020-06-27 14:47:04 +0900

    メッセージ送信機能の実装

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,8 @@
 class Message < ApplicationRecord
-  belong_to :group
-  belong_to :uses
+  belongs_to :group
+  belongs_to :user
 
-  validates :content, presence: true, unless: image?
+  validates :content, presence: true, unless: :image?
 
   mount_uploader :image, ImageUploader
 end

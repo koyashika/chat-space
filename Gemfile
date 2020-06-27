d9ce15ec7c1ed5ad369477bcb09adb4e2cedc075
koyashika <u_wh.sunnnny.xx@softbank.ne.jp>
2020-06-27 11:45:08 +0900

    メッセージ送信機能の実装

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ ruby '2.6.5'
 
 gem 'devise'
 gem 'pry-rails'
+gem 'carrierwave'
+gem 'mini_magick'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.0'
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,8 @@
 class Message < ApplicationRecord
   belong_to :group
   belong_to :uses
+
   validates :content, presence: true, unless: image?
+
+  mount_uploader :image, ImageUploader
 end

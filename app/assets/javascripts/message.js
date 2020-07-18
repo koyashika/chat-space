e0997cabb4f379be3c70e03017d057ed5cf467cc
koyashika <u_wh.sunnnny.xx@softbank.ne.jp>
2020-07-18 12:44:36 +0900

    返ってきたJSONをdoneメソッドで受取り、HTMLを作成

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,46 @@
 $(function(){
-  $(".Form").on("submit", function(e){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html =
+        `<div class="MessageBox">
+          <div class="message-info">
+            <div class="message-info__user-name">
+              ${message.user_name}
+            </div>
+            <div class="Message-info__post-date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message">
+            <p class="message__message-contents">
+              ${message.content}
+            </p>
+            <img class="Message__image" src="${message.image}">
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="MessageBox">
+        <div class="message-info">
+            <div class="message-info__user-name">
+            ${message.user_name}
+          </div>
+          <div class="message-info__post-date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message">
+          <p class="message__message-contents">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
+
+  $('.Form').on('submit', function(e){
     e.preventDefault();
     let formData = new FormData(this);
     let url = $(this).attr('action');
@@ -10,6 +51,9 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
     })
   });
 });

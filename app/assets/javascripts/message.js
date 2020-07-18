08f03a336e8c42810e9fc1ca43dc63575187f505
koyashika <u_wh.sunnnny.xx@softbank.ne.jp>
2020-07-18 14:48:54 +0900

    クラス名の修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,36 +2,36 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       let html =
-        `<div class="MessageBox">
-          <div class="message-info">
-            <div class="message-info__user-name">
+        `<div class="Message-box">
+          <div class="Message-box__message-info">
+            <div class="Message-box__message-info__user-name">
               ${message.user_name}
             </div>
-            <div class="Message-info__post-date">
+            <div class="Message-box__message-info__post-date">
               ${message.created_at}
             </div>
           </div>
-          <div class="message">
-            <p class="message__message-contents">
+          <div class="Message-box__message">
+            <p class="Message-box__message__message-contents">
               ${message.content}
             </p>
-            <img class="Message__image" src="${message.image}">
+            <img class="Message-box__message__image" src="${message.image}">
           </div>
         </div>`
       return html;
     } else {
       let html =
-      `<div class="MessageBox">
-        <div class="message-info">
-            <div class="message-info__user-name">
+      `<div class="Message-box__Message-box">
+        <div class="Message-box__message-info">
+            <div class="Message-box__message-info__user-name">
             ${message.user_name}
           </div>
-          <div class="message-info__post-date">
+          <div class="Message-box__message-info__post-date">
             ${message.created_at}
           </div>
         </div>
-        <div class="message">
-          <p class="message__message-contents">
+        <div class="Message-box__message">
+          <p class="Message-box__message__message-contents">
             ${message.content}
           </p>
         </div>
@@ -54,8 +54,9 @@ $(function(){
     })
     .done(function(data){
       let html = buildHTML(data);
-      $('.MessageField').append(html);      
+      $('.Message-list').append(html);      
       $('form')[0].reset();
+      $('.Message-list').animate({ scrollTop: $('.Message-list')[0].scrollHeight});
     })
   });
 });

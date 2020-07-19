29f37356e3310c0e7439f0c8e6bf41fcd553fd2a
koyashika <u_wh.sunnnny.xx@softbank.ne.jp>
2020-07-19 15:57:51 +0900

    取得した投稿データの表示をさせる

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -61,6 +61,25 @@ $(function(){
     })
     .fail(function() {
       alert("メッセージ送信に失敗しました");
+    });
   });
-  });
+  let reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    let last_message_id = $('.MessageBox:last').data("message-id") || 0;
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      console.log('success');
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
 });
